fb1ba5b9247d1f13c95fe0a88005a10f1691e63b
arieldavis22 <amanichai100@gmail.com>
2022-08-15 19:09:48 -0400

    linklist1

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Ariel", 55.99);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for(int i =0; i< intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for(int i =0; i< intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+    }
+}
